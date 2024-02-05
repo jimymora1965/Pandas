386ec3552a6a0f32185e6be9aa80bbc6dbe08398
Jimy Mora <jimymora@gmail.com>
2024-02-05 18:11:33 -0500

    imprimr solo una columna del Dataframe

--- Dataframes/dataframe_diccionario.py
+++ Dataframes/dataframe_diccionario.py
@@ -1,10 +1,28 @@
-import pandas as pd
+""" import pandas as pd
 
 datos = {
     "Nombre": pd.Series(["Jimy", "Erica", "Christian", "Valentina"]),
-    "Edad": pd.Series(["58","45","27","24"]),
-    "Nacido":pd.Series(["Buenaventura","Valledupar","San Andres Isla", ])
-    }
+    "Edad": pd.Series([58, 45, 27, 24]),
+    "Nacido": pd.Series(["Buenaventura", "Valledupar", "San Andres Isla", ""]),
+    "Estudios": pd.Series(["Primaria", "Chef", "", ""])
+}
 
 data = pd.DataFrame(datos)
 print(data)
+ """
+
+print("Para imprimir solo la columna Nombre: ")
+import pandas as pd
+datos = {
+    "Nombre": pd.Series(["Jimy", "Erica", "Christian", "Valentina"]),
+    "Edad": pd.Series([58, 45, 27, 24]),
+    "Nacido": pd.Series(["Buenaventura", "Valledupar", "San Andres Isla", ""]),
+    "Estudios": pd.Series(["Primaria", "Chef", "", ""])
+}
+
+data = pd.DataFrame(datos)
+
+# Imprimir solo la columna "Nombre"
+nombres = data["Nombre"]
+print("Nombres:")
+print(nombres)
